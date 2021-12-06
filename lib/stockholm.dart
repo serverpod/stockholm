e15d35ed7ab42b44f18dc68355b4540ed6bd2cd7
Viktor Lidholt <viktorlidholt@gmail.com>
2021-12-06 23:18:35 +0100

    Adds progress indicator

--- lib/stockholm.dart
+++ lib/stockholm.dart
@@ -5,6 +5,7 @@ export 'src/dialog.dart';
 export 'src/dropdown_button.dart';
 export 'src/list_tile.dart';
 export 'src/popup_menu_button.dart';
+export 'src/progress_dart.yaml.dart';
 export 'src/property_inspector.dart';
 export 'src/theme.dart';
 export 'src/shadows.dart';
