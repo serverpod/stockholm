b3c40d7b39a993dea0ad349175f7e441b8d9c07c
Viktor Lidholt <viktorlidholt@gmail.com>
2021-12-08 08:23:05 +0100

    Adds delete button to list tile

--- lib/src/list_tile.dart
+++ lib/src/list_tile.dart
@@ -1,32 +1,75 @@
 import 'package:flutter/material.dart';
 
-class StockholmListTile extends StatelessWidget {
+class StockholmListTile extends StatefulWidget {
   final Widget child;
   final bool selected;
   final VoidCallback? onPressed;
+  final VoidCallback? onDelete;
 
   const StockholmListTile({
     required this.child,
     this.selected = false,
     this.onPressed,
+    this.onDelete,
     Key? key,
   }) : super(key: key);
 
   @override
+  _StockholmListTileState createState() => _StockholmListTileState();
+}
+
+class _StockholmListTileState extends State<StockholmListTile> {
+  bool _hover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-          decoration: selected
-              ? BoxDecoration(
-                  color: Theme.of(context).highlightColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(6)),
+    var content = widget.child;
+    if (widget.onDelete != null) {
+      content = Row(
+        children: [
+          Expanded(child: content),
+          _hover
+              ? GestureDetector(
+                  onTap: widget.onDelete,
+                  child: const Icon(
+                    Icons.close,
+                    size: 16,
+                  ),
                 )
-              : null,
-          child: child,
+              : const SizedBox(
+                  width: 16,
+                  height: 16,
+                ),
+        ],
+      );
+    }
+
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          _hover = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _hover = false;
+        });
+      },
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: widget.onPressed,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+            decoration: widget.selected
+                ? BoxDecoration(
+                    color: Theme.of(context).highlightColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(6)),
+                  )
+                : null,
+            child: content,
+          ),
         ),
       ),
     );
