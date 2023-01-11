3f1b80268eac771dbb27295f1ba5b9a8608cc918
Viktor Lidholt <viktorlidholt@gmail.com>
2023-01-11 14:45:12 +0100

    Adds flat button

--- lib/src/button.dart
+++ lib/src/button.dart
@@ -63,3 +63,70 @@ class StockholmButton extends StatelessWidget {
     );
   }
 }
+
+class StockholmFlatButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final Widget child;
+  final EdgeInsets padding;
+  final bool large;
+  final bool important;
+  final bool enabled;
+
+  const StockholmFlatButton({
+    Key? key,
+    required this.onPressed,
+    required this.child,
+    this.padding = const EdgeInsets.symmetric(
+      horizontal: 12,
+    ),
+    this.large = false,
+    this.important = false,
+    this.enabled = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    Color textColor;
+    if (enabled && onPressed != null) {
+      if (important) {
+        textColor = theme.buttonTheme.colorScheme!.onPrimary;
+      } else {
+        textColor = theme.buttonTheme.colorScheme!.onSurface;
+      }
+    } else {
+      textColor = theme.disabledColor;
+    }
+
+    return MaterialButton(
+      // textTheme: textTheme,
+      color: important
+          ? theme.colorScheme.primary
+          : theme.buttonTheme.colorScheme!.surface,
+      hoverColor: Theme.of(context).hoverColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+        side: BorderSide(color: Theme.of(context).dividerColor, width: 1),
+      ),
+      onPressed: enabled ? onPressed : null,
+      child: Container(
+        alignment: Alignment.center,
+        height: large ? 24 : 16,
+        padding: padding,
+        child: DefaultTextStyle(
+          style: large
+              ? theme.textTheme.button!.copyWith(color: textColor)
+              : theme.textTheme.bodyText2!.copyWith(color: textColor),
+          child: child,
+        ),
+      ),
+      elevation: 0,
+      hoverElevation: 0,
+      highlightElevation: 0,
+      focusElevation: 0,
+      minWidth: 32,
+      padding: EdgeInsets.zero,
+    );
+  }
+}
